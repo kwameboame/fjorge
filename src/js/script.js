ef7f4115cdd5c4ce9352ea7b54e21d46090fdd54
Sosthenes Kwame Boame <kwame.boame@live.com>
2023-10-19 18:51:12 +0000

    added js to handle mobile menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,18 @@
-/**************************
-  * SCRIPT
-**************************/
+// Is script loaded?
 console.log('script file is connected');
+
+
+// SCRIPT FOR MOBILE MENU TOGGLE
+// Get HTML Elements
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector("#mobile-menu");
+
+// Add Event Listener
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  // Toggle the visibility of the menu icons based on state
+  const menuOpenIcon = document.querySelector(".menu-open");
+  const menuClosedIcon = document.querySelector(".menu-closed");
+  menuOpenIcon.classList.toggle("hidden");
+  menuClosedIcon.classList.toggle("hidden");
+});
