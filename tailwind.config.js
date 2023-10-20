04d2abdbdbe825aeb34e7014113368df312057b1
Sosthenes Kwame Boame <kwame.boame@live.com>
2023-10-20 01:41:17 +0000

    added more styling to benefits & included Rift font in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'rift-regular': ['Rift-Regular', 'Arial'], // Add the new font family here
+      'rift-medium': ['Rift-Medium', 'Arial'], 
+      'rift-light': ['Rift-Light', 'Arial'], // Add the new font family here
+      'rift-bold': ['Rift-Bold', 'Arial']
+    },
     extend: {
-      colors: {'cust-darkblue': '#014152', },
+      colors: {
+        'cust-darkblue': '#014152', 
+      },
+      textColor: {
+        'cust-darkblue': '#416F7E',
+      },
     },
   },
   plugins: [],
