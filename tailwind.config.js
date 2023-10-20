63f2532a1d7814acab4a74c8cce64fa841c62e42
Sosthenes Kwame Boame <kwame.boame@live.com>
2023-10-20 13:39:28 +0000

    style fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
     },
     extend: {
       colors: {
-        'cust-darkblue': '#014152', 
+        'cust-footer-bg': '#014152', 
+        'cust-darkerblue': '#003E52', 
       },
       textColor: {
         'cust-darkblue': '#416F7E',
